5edda4e5721e2cad29ef0e0219ca10d8de8d6f17
mnbellouni <mnbellouni@gmail.com>
2026-04-26 22:40:26 +0200

    Initial Commit

---
+++ ColisApp/ColisAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColisAppApp.swift
+//  ColisApp
+//
+//  Created by Nadjib Bellouni on 26/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ColisAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColisAppTests/ColisAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  ColisAppTests.swift
+//  ColisAppTests
+//
+//  Created by Nadjib Bellouni on 26/04/2026.
+//
+
+import Testing
+@testable import ColisApp
+
+struct ColisAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
